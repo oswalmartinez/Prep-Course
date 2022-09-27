7c385825dcec85b2d2b865a7228c75717b91fb04
Oswaldo Martinez <ooswalmarts045@gmail.com>
2022-09-26 20:19:42 -0500

    Prueba termaniada JS Extra

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,22 +10,48 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
-}
+  var matriz = [];
 
-
+  // key = propiedad = calve
+  for(var key in objeto){
+   matriz.push([key,objeto[key]])
+  }
+    return matriz
+  }
+  
 function numberOfCharacters(string) {
   //La función recibe un string. Recorre el srting y devuelve el caracter con el número de veces que aparece 
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
-}
+  var cantCaracteres = {};
+  for (var i = 0;  i < string.length; i++) {
+    if(cantCaracteres.hasOwnProperty(string[i])){
+      cantCaracteres[string[i]]++;
+    } else {
+      cantCaracteres[string[i]] = 1;
+    }
 
+  }
+  return cantCaracteres;
+}
 
 function capToFront(s) {
   //Realiza una función que reciba como parámetro un string y mueva todas las letras mayúsculas
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var mayusc = "";
+  var minusc = "";
+  for(var i = 0; i < s.length; i++) {
+     if(s[i] === s[i].toUpperCase()){
+      mayusc += s[i];
+  } else {
+    minusc += s[i];
+  }
+
+};
+return mayusc + minusc;
 }
 
 
@@ -35,6 +61,30 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  var frase = str;
+  console.log(frase);
+
+  var frasePorPalabras = frase.split(" ");
+  console.log(frasePorPalabras);
+
+  var fraseADevolver = [];
+
+for(var i = 0; i < frasePorPalabras.length; i++) {
+  var palabra = frasePorPalabras[i];
+  var palabraSeparada = palabra.split("");
+  console.log("palabraSeparada",palabraSeparada);
+
+  var palabraRevertida = palabraSeparada.reverse();
+  console.log("palabraRevertida",palabraRevertida);
+
+  var palabraUnida = palabraRevertida.join("");
+  console.log(palabraUnida);
+
+  fraseADevolver.push(palabraUnida);
+}
+console.log(fraseADevolver);
+
+return fraseADevolver.join(" ");
 } 
 
 
@@ -43,6 +93,13 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  let numeroStr = numero.toString();
+  var espejo = "";
+   for (let i = numeroStr.length-1; i >= 0; i--) {
+     espejo += numeroStr[i];      
+  }
+if(numeroStr === espejo) return "Es capicua";
+return "No es capicua";
 }
 
 
@@ -50,13 +107,26 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+let nuevaCadena = "";
+for(let letra of  cadena) {
+  if(letra != "a"&& letra != "b" && letra != "c" ) {
+    nuevaCadena += letra
+  }
 }
+return nuevaCadena;
 
+
+  console.log(deleteAbc("abcefgh"));
+}
 
 function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  return arr.sort(function (a, b) {
+    return a.length - b.length;
+  }) ;
+
 }
 
 
@@ -66,6 +136,16 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+  let nuveoarray = [];
+  for( let e of arreglo1) {
+     for(let i of arreglo2){
+        if(e === i) {
+         nuveoarray.push(e);
+        }
+       }
+  }
+ 
+return nuveoarray;
 }
 
 
